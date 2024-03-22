8fc555f66134c6995dafce0a8a50d8156cd1c8e9
Alberto Verna <alberto.verna@hotmail.com>
2024-03-22 13:38:59 +0100

    Create document_store

---
+++ document_store/src/main/kotlin/it/polito/wa2/g07/document_store/DocumentStoreApplication.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2.g07.document_store
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DocumentStoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<DocumentStoreApplication>(*args)
+}
