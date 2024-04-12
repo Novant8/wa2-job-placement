59609d02daf650f6018e1ce734636b172396d315
Andrea Sordello <sordello.andrea@gmail.com>
2024-04-12 13:32:46 +0200

    setup module

---
+++ CRM/src/test/kotlin/it/polito/wa2/g07/crm/CrmApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.wa2.g07.crm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
