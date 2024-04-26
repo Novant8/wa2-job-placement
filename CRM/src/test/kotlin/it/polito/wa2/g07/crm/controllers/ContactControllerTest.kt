ca21a76a307ff123ddac16d5b9e55e8313a22766
Alberto Verna <alberto.verna@hotmail.com>
2024-04-26 15:18:20 +0200

    Add unit tests for POST, edit integration tests to match edge-case edits

--- CRM/src/test/kotlin/it/polito/wa2/g07/crm/controllers/ContactControllerTest.kt
+++ CRM/src/test/kotlin/it/polito/wa2/g07/crm/controllers/ContactControllerTest.kt
@@ -1,10 +1,14 @@
 package it.polito.wa2.g07.crm.controllers
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import io.mockk.verify
 import it.polito.wa2.g07.crm.dtos.*
 import it.polito.wa2.g07.crm.entities.ContactCategory
+import it.polito.wa2.g07.crm.exceptions.ContactNotFoundException
+import it.polito.wa2.g07.crm.exceptions.DuplicateAddressException
 import it.polito.wa2.g07.crm.services.ContactService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
@@ -14,6 +18,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
@@ -24,16 +29,15 @@ class ContactControllerTest(@Autowired val mockMvc: MockMvc) {
     @MockkBean
     private lateinit var contactService: ContactService
 
+    private val mockEmailDTO = EmailDTO("mario.rossi@example.org")
+    private val mockTelephoneDTO = TelephoneDTO("34242424242")
+    private val mockDwellingDTO = DwellingDTO("Via Roma, 18", "Torino", "TO", "IT")
     private val mockContactDTO = ContactDTO(
         1L,
         "Mario",
         "Rossi",
         ContactCategory.CUSTOMER,
-        listOf(
-            EmailDTO("mario.rossi@example.org"),
-            TelephoneDTO("34242424242"),
-            DwellingDTO("Via Roma, 18", "Torino", "TO", "IT")
-        ),
+        listOf(mockEmailDTO, mockTelephoneDTO, mockDwellingDTO),
         "RSSMRA70A01L219K"
     )
 
@@ -157,8 +161,220 @@ class ContactControllerTest(@Autowired val mockMvc: MockMvc) {
                 }
                 .andExpect {
                     status { isBadRequest() }
+                }
+        }
+
+    }
+
+    @Nested
+    inner class PostContactTests {
+
+        private val jsonMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
+
+        @BeforeEach
+        fun initMocks() {
+            every { contactService.create(any(CreateContactDTO::class)) } answers { firstArg<CreateContactDTO>().toEntity().toContactDto() }
+            every { contactService.insertEmail(any(Long::class), any(String::class)) } throws ContactNotFoundException("Contact does not exist")
+            every { contactService.insertEmail(mockContactDTO.id, any(String::class)) } returns Unit
+            every { contactService.insertEmail(mockContactDTO.id, mockEmailDTO.email) } throws DuplicateAddressException("Mail already associated to contact")
+        }
+
+        @Test
+        fun saveContact_noAddresses() {
+            val createContactDTO = CreateContactDTO(
+                "Luigi",
+                "Verdi",
+                ContactCategory.PROFESSIONAL.name,
+                "VRDLGU70A01L219G",
+                addresses = listOf()
+            )
+
+            mockMvc
+                .post("/API/contacts") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = jsonMapper.writeValueAsString(createContactDTO)
+                }
                 .andExpect{
-                    status().isBadRequest
+                    status { isCreated() }
+                    content {
+                        jsonPath("$.name") { value(createContactDTO.name) }
+                        jsonPath("$.surname") { value(createContactDTO.surname) }
+                        jsonPath("$.category") { value(createContactDTO.category) }
+                        jsonPath("$.ssn") { value(createContactDTO.ssn) }
+                        jsonPath("$.addresses") { isEmpty() }
+                    }
+                }
+        }
+
+        @Test
+        fun saveContact_noSSN() {
+            val createContactDTO = CreateContactDTO(
+                "Luigi",
+                "Verdi",
+                ContactCategory.PROFESSIONAL.name,
+                null,
+                addresses = listOf()
+            )
+
+            mockMvc
+                .post("/API/contacts") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = jsonMapper.writeValueAsString(createContactDTO)
+                }
+                .andExpect{
+                    status { isCreated() }
+                    content {
+                        jsonPath("$.name") { value(createContactDTO.name) }
+                        jsonPath("$.surname") { value(createContactDTO.surname) }
+                        jsonPath("$.category") { value(createContactDTO.category) }
+                        jsonPath("$.ssn") { doesNotExist() }
+                        jsonPath("$.addresses") { isEmpty() }
+                    }
+                }
+        }
+
+        @Test
+        fun saveContact_withAddresses() {
+            val mailDTO = EmailDTO("luigi.verdi@example.org")
+            val telephoneDTO = TelephoneDTO("34798989898")
+            val dwellingDTO = DwellingDTO("Via Roma, 19", "Torino", "TO", "IT")
+            val createContactDTO = CreateContactDTO(
+                "Luigi",
+                "Verdi",
+                ContactCategory.PROFESSIONAL.name,
+                "VRDLGU70A01L219G",
+                addresses = listOf(mailDTO, telephoneDTO, dwellingDTO)
+            )
+
+            mockMvc
+                .post("/API/contacts") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = jsonMapper.writeValueAsString(createContactDTO)
+                }
+                .andExpect{
+                    status { isCreated() }
+                    content {
+                        jsonPath("$.name") { value(createContactDTO.name) }
+                        jsonPath("$.surname") { value(createContactDTO.surname) }
+                        jsonPath("$.category") { value(createContactDTO.category) }
+                        jsonPath("$.ssn") { value(createContactDTO.ssn) }
+                        jsonPath("$.addresses") { isArray() }
+                        jsonPath("$.addresses[0].email") { value(mailDTO.email) }
+                        jsonPath("$.addresses[1].phoneNumber") { value(telephoneDTO.phoneNumber)}
+                        jsonPath("$.addresses[2].street") { value(dwellingDTO.street) }
+                        jsonPath("$.addresses[2].city") { value(dwellingDTO.city) }
+                        jsonPath("$.addresses[2].district") { value(dwellingDTO.district) }
+                        jsonPath("$.addresses[2].country") { value(dwellingDTO.country) }
+                    }
+                }
+        }
+
+        @Test
+        fun saveContact_invalidName() {
+            for (name in listOf(null, "")) {
+                val createContactDTO = CreateContactDTO(
+                    name,
+                    "Verdi",
+                    ContactCategory.PROFESSIONAL.name,
+                    "VRDLGU70A01L219G",
+                    addresses = listOf()
+                )
+
+                mockMvc
+                    .post("/API/contacts") {
+                        contentType = MediaType.APPLICATION_JSON
+                        content = jsonMapper.writeValueAsString(createContactDTO)
+                    }
+                    .andExpect{
+                        status { isUnprocessableEntity() }
+                    }
+            }
+        }
+
+        @Test
+        fun saveContact_invalidSurname() {
+            for (surname in listOf(null, "")) {
+                val createContactDTO = CreateContactDTO(
+                    "Luigi",
+                    surname,
+                    ContactCategory.PROFESSIONAL.name,
+                    "VRDLGU70A01L219G",
+                    addresses = listOf()
+                )
+
+                mockMvc
+                    .post("/API/contacts") {
+                        contentType = MediaType.APPLICATION_JSON
+                        content = jsonMapper.writeValueAsString(createContactDTO)
+                    }
+                    .andExpect {
+                        status { isUnprocessableEntity() }
+                    }
+            }
+        }
+
+        @Test
+        fun saveContact_invalidCategory() {
+            val createContactDTO = CreateContactDTO(
+                "Luigi",
+                "Verdi",
+                "I am not valid",
+                "VRDLGU70A01L219G",
+                addresses = listOf()
+            )
+
+            mockMvc
+                .post("/API/contacts") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = jsonMapper.writeValueAsString(createContactDTO)
+                }
+                .andExpect{
+                    status { isCreated() }
+                    content {
+                        jsonPath("$.category") { value(ContactCategory.UNKNOWN.name) }
+                    }
+                }
+        }
+
+        @Test
+        fun insertMail_success() {
+            val id = mockContactDTO.id
+            val mailJson = "{ \"email\": \"mario.rossi2@gmail.com\" }"
+            mockMvc
+                .post("/API/contacts/$id/email") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = mailJson
+                }
+                .andExpect{
+                    status { isCreated() }
+                }
+        }
+
+        @Test
+        fun insertMail_contactNotFound() {
+            val id = mockContactDTO.id + 1
+            val mailJson = "{ \"email\": \"mario.rossi2@gmail.com\" }"
+            mockMvc
+                .post("/API/contacts/$id/email") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = mailJson
+                }
+                .andExpect{
+                    status { isNotFound() }
+                }
+        }
+
+        @Test
+        fun insertMail_duplicateMail() {
+            val id = mockContactDTO.id
+            val mailJson = "{ \"email\": \"${mockEmailDTO.email}\" }"
+            mockMvc
+                .post("/API/contacts/$id/email") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = mailJson
+                }
+                .andExpect{
+                    status { isNotModified() }
                 }
         }
 
--- CRM/src/test/kotlin/it/polito/wa2/g07/crm/services/ContactServiceTest.kt
+++ CRM/src/test/kotlin/it/polito/wa2/g07/crm/services/ContactServiceTest.kt
@@ -1,13 +1,11 @@
 package it.polito.wa2.g07.crm.services
 
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
-import it.polito.wa2.g07.crm.dtos.ContactFilterBy
-import it.polito.wa2.g07.crm.dtos.toContactDto
-import it.polito.wa2.g07.crm.dtos.toReducedContactDTO
+import io.mockk.*
+import it.polito.wa2.g07.crm.dtos.*
 import it.polito.wa2.g07.crm.entities.*
 import it.polito.wa2.g07.crm.exceptions.ContactNotFoundException
+import it.polito.wa2.g07.crm.exceptions.DuplicateAddressException
+import it.polito.wa2.g07.crm.repositories.AddressRepository
 import it.polito.wa2.g07.crm.repositories.ContactRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
@@ -28,17 +26,25 @@ class ContactServiceTest {
         ContactCategory.CUSTOMER,
         "RSSMRA70A01L219K"
     )
+    private val mockMail = Email("mario.rossi@example.org")
+    private val mockTelephone = Telephone("34242424242")
+    private val mockDwelling = Dwelling("Via Roma, 18", "Torino", "TO", "IT")
+
     init {
         mockContact.contactId = 1L
+        mockMail.id = 1L
+        mockTelephone.id = 2L
+        mockDwelling.id = 3L
         mockContact.addresses = mutableSetOf(
-            Email("mario.rossi@example.org"),
-            Telephone("34242424242"),
-            Dwelling("Via Roma, 18", "Torino", "TO", "IT")
+            mockMail,
+            mockTelephone,
+            mockDwelling
         )
     }
 
     private val contactRepository = mockk<ContactRepository>()
-    private val service = ContactServiceImpl(contactRepository)
+    private val addressRepository = mockk<AddressRepository>()
+    private val service = ContactServiceImpl(contactRepository, addressRepository)
 
     @Nested
     inner class GetContactTests {
@@ -50,7 +56,7 @@ class ContactServiceTest {
         fun initMocks() {
             every { contactRepository.findAll(any(Pageable::class)) } returns pageImpl
             every { contactRepository.findAllByFullNameLike(any(String::class), any(Pageable::class)) } returns pageImpl
-            every { contactRepository.findAllBySSN(any(String::class), any(Pageable::class)) } returns pageImpl
+            every { contactRepository.findAllBySsn(any(String::class), any(Pageable::class)) } returns pageImpl
             every { contactRepository.findAllByEmail(any(String::class), any(Pageable::class)) } returns pageImpl
             every { contactRepository.findAllByTelephone(any(String::class), any(Pageable::class)) } returns pageImpl
             every { contactRepository.findAllByDwellingLike(any(String::class), any(Pageable::class)) } returns pageImpl
@@ -72,7 +78,7 @@ class ContactServiceTest {
         fun getContacts_stringFilters() {
             val filterFunctionsMap: Map<ContactFilterBy, (String, Pageable) -> Page<Contact>> = mapOf(
                 ContactFilterBy.FULL_NAME to contactRepository::findAllByFullNameLike,
-                ContactFilterBy.SSN to contactRepository::findAllBySSN,
+                ContactFilterBy.SSN to contactRepository::findAllBySsn,
                 ContactFilterBy.EMAIL to contactRepository::findAllByEmail,
                 ContactFilterBy.TELEPHONE to contactRepository::findAllByTelephone,
                 ContactFilterBy.ADDRESS to contactRepository::findAllByDwellingLike
@@ -118,5 +124,201 @@ class ContactServiceTest {
                 service.getContactById(mockContact.contactId + 1)
             }
         }
+    }
+
+    @Nested
+    inner class PostContactTests {
+
+        val contactSlot = slot<Contact>()
+
+        @BeforeEach
+        fun initMocks() {
+            every { contactRepository.save(capture(contactSlot)) } answers { firstArg<Contact>() }
+            every { contactRepository.findById(any(Long::class)) } returns Optional.empty()
+            every { contactRepository.findById(mockContact.contactId) } returns Optional.of(mockContact)
+            every { addressRepository.findMailAddressByMail(any(String::class)) } returns Optional.empty()
+            every { addressRepository.findMailAddressByMail(mockMail.email) } returns Optional.of(mockMail)
+            every { addressRepository.findTelephoneAddressByTelephoneNumber(any(String::class)) } returns Optional.empty()
+            every { addressRepository.findTelephoneAddressByTelephoneNumber(mockTelephone.number) } returns Optional.of(mockTelephone)
+            every { addressRepository.findDwellingAddressByStreet(any(String::class), any(String::class), any(String::class), any(String::class)) } returns Optional.empty()
+            every { addressRepository.findDwellingAddressByStreet(mockDwelling.street, mockDwelling.city, mockDwelling.district, mockDwelling.country) } returns Optional.of(mockDwelling)
+        }
+
+        @Test
+        fun create_noSSN_noAddresses() {
+            val createContactDTO = CreateContactDTO(
+                "Luigi",
+                "Verdi",
+                ContactCategory.PROFESSIONAL.name,
+                null,
+                listOf()
+            )
+
+            val result = service.create(createContactDTO)
+
+            val expectedDTO = ContactDTO(
+                0L,
+                createContactDTO.name!!,
+                createContactDTO.surname!!,
+                ContactCategory.PROFESSIONAL,
+                createContactDTO.addresses,
+                createContactDTO.ssn
+            )
+            assertEquals(result, expectedDTO)
+            verify { contactRepository.save(any(Contact::class)) }
+        }
+
+        @Test
+        fun create_SSN_noAddresses() {
+            val createContactDTO = CreateContactDTO(
+                "Luigi",
+                "Verdi",
+                ContactCategory.PROFESSIONAL.name,
+                "VRDLGU70A01L219G",
+                addresses = listOf()
+            )
+
+            val result = service.create(createContactDTO)
+
+            val expectedDTO = ContactDTO(
+                0L,
+                createContactDTO.name!!,
+                createContactDTO.surname!!,
+                ContactCategory.PROFESSIONAL,
+                createContactDTO.addresses,
+                createContactDTO.ssn
+            )
+            assertEquals(result, expectedDTO)
+            verify { contactRepository.save(any(Contact::class)) }
+        }
+
+        @Test
+        fun create_withAddresses() {
+            val createContactDTO = CreateContactDTO(
+                "Luigi",
+                "Verdi",
+                ContactCategory.PROFESSIONAL.name,
+                "VRDLGU70A01L219G",
+                listOf(
+                    EmailDTO("luigi.verdi@example.org"),
+                    TelephoneDTO("34798989898"),
+                    DwellingDTO("Via Roma, 19", "Torino", "TO", "IT")
+                )
+            )
+
+            val result = service.create(createContactDTO)
+
+            val expectedDTO = ContactDTO(
+                0L,
+                createContactDTO.name!!,
+                createContactDTO.surname!!,
+                ContactCategory.PROFESSIONAL,
+                createContactDTO.addresses,
+                createContactDTO.ssn
+            )
+            assertEquals(result, expectedDTO)
+            verify { contactRepository.save(any(Contact::class)) }
+        }
+
+        @Test
+        fun create_existingAddresses() {
+            val createContactDTO = CreateContactDTO(
+                "Maria",
+                "Rossi",
+                ContactCategory.PROFESSIONAL.name,
+                "VRDLGU70A01L219G",
+                listOf(
+                    EmailDTO(mockMail.email),
+                    TelephoneDTO(mockTelephone.number),
+                    DwellingDTO(mockDwelling.street, mockDwelling.city, mockDwelling.district, mockDwelling.country)
+                )
+            )
+
+            val result = service.create(createContactDTO)
+
+            val expectedDTO = ContactDTO(
+                0L,
+                createContactDTO.name!!,
+                createContactDTO.surname!!,
+                ContactCategory.PROFESSIONAL,
+                createContactDTO.addresses,
+                createContactDTO.ssn
+            )
+            assertEquals(result, expectedDTO)
+            verify { contactRepository.save(any(Contact::class)) } 
+        }
+
+        @Test
+        fun create_invalidCategory() {
+            val createContactDTO = CreateContactDTO(
+                "Luigi",
+                "Verdi",
+                "I am not valid",
+                "VRDLGU70A01L219G",
+                listOf(
+                    EmailDTO("luigi.verdi@example.org"),
+                    TelephoneDTO("34798989898"),
+                    DwellingDTO("Via Roma, 19", "Torino", "TO", "IT")
+                )
+            )
+
+            val result = service.create(createContactDTO)
+
+            val expectedDTO = ContactDTO(
+                0L,
+                createContactDTO.name!!,
+                createContactDTO.surname!!,
+                ContactCategory.UNKNOWN,
+                createContactDTO.addresses,
+                createContactDTO.ssn
+            )
+            assertEquals(result, expectedDTO)
+            verify { contactRepository.save(any(Contact::class)) }
+        }
+
+        @Test
+        fun insertEmail_success() {
+            val id = mockContact.contactId
+            val newMail = "mario.rossi@gmail.com"
+            service.insertEmail(id, newMail)
+
+            verify { contactRepository.save(any(Contact::class)) }
+            assertNotNull(contactSlot.captured.addresses.find { it is Email && it.email == newMail })
+        }
+
+        @Test
+        fun insertEmail_existingMail() {
+            val existingMail = "existing.mail@example.org"
+            val existingMailEntity = Email(existingMail)
+            existingMailEntity.id = 2L
+            every { addressRepository.findMailAddressByMail(existingMail) } returns Optional.of(existingMailEntity)
+
+            val id = mockContact.contactId
+            service.insertEmail(id, existingMail)
+
+            verify { contactRepository.save(any(Contact::class)) }
+            assertEquals(contactSlot.captured.addresses.find { it is Email && it.email == existingMail }?.id, existingMailEntity.id)
+        }
+
+        @Test
+        fun insertEmail_mailAlreadyAssociatedToContact() {
+            val id = mockContact.contactId
+            val newMail = mockMail.email
+            assertThrows<DuplicateAddressException> {
+                service.insertEmail(id, newMail)
+            }
+            verify { contactRepository.save(any(Contact::class)) wasNot called }
+        }
+
+        @Test
+        fun insertEmail_invalidUser() {
+            val id = mockContact.contactId + 1
+            val newMail = "mario.rossi@gmail.com"
+            assertThrows<ContactNotFoundException> {
+                service.insertEmail(id, newMail)
+            }
+            verify { contactRepository.save(any(Contact::class)) wasNot called }
+        }
+
     }
 }
