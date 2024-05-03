e4224c88cfc44490efe7cb4750b5530f55afe8ee
SalvatoreCavallaro1 <135614562+SalvatoreCavallaro1@users.noreply.github.com>
2024-05-03 15:53:50 +0200

    service update

--- CRM/src/main/kotlin/it/polito/wa2/g07/crm/services/lab03/ProfessionalService.kt
+++ CRM/src/main/kotlin/it/polito/wa2/g07/crm/services/lab03/ProfessionalService.kt
@@ -1,4 +1,8 @@
 package it.polito.wa2.g07.crm.services.lab03
 
+import it.polito.wa2.g07.crm.dtos.lab03.CreateProfessionalDTO
+import it.polito.wa2.g07.crm.dtos.lab03.ProfessionalDTO
+
 interface ProfessionalService {
+    fun create(professional: CreateProfessionalDTO): ProfessionalDTO
 }
