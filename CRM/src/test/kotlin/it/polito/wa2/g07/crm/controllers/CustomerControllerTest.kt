478fa7feebfc95fc24c11a2bbc433a7683a32e0b
Andrea Sordello <sordello.andrea@gmail.com>
2024-05-03 15:23:02 +0200

    backbone of Test

---
+++ CRM/src/test/kotlin/it/polito/wa2/g07/crm/controllers/CustomerControllerTest.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2.g07.crm.controllers
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+
+
+@WebMvcTest(CustomerController::class)
+class CustomerControllerTest(@Autowired val mockMvc: MockMvc) {
+
+}
---
+++ CRM/src/test/kotlin/it/polito/wa2/g07/crm/controllers/JobOfferControllerTest.kt
@@ -0,0 +1,10 @@
+package it.polito.wa2.g07.crm.controllers
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+
+@WebMvcTest(JobOfferController::class)
+class JobOfferControllerTest(@Autowired val mockMvc: MockMvc) {
+
+}
