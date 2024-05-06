54727c9250ec44a8c7459ece7f09963c24a0d419
SalvatoreCavallaro1 <135614562+SalvatoreCavallaro1@users.noreply.github.com>
2024-05-07 01:39:46 +0200

    update CreateProfessionalDTO, ProfessionalDTO

--- CRM/src/main/kotlin/it/polito/wa2/g07/crm/dtos/lab03/CreateProfessionalDTO.kt
+++ CRM/src/main/kotlin/it/polito/wa2/g07/crm/dtos/lab03/CreateProfessionalDTO.kt
@@ -1,16 +1,25 @@
 package it.polito.wa2.g07.crm.dtos.lab03
 
 import it.polito.wa2.g07.crm.dtos.lab02.CreateContactDTO
+import it.polito.wa2.g07.crm.dtos.lab02.toEntity
+import it.polito.wa2.g07.crm.entities.lab03.EmploymentState
+import it.polito.wa2.g07.crm.entities.lab03.Professional
 
 data class CreateProfessionalDTO
     (
-            val contactInfo: CreateContactDTO,
-            val location: String,
-            val skills: Set<String>,
-            var daily_rate: Double,
-            var notes: String?
+        val contactInfo: CreateContactDTO,
+        val location: String,
+        val skills: Set<String>,
+        var dailyRate: Double,
+        var employmentState: EmploymentState,
+        var notes: String?
             )
-{
 
-
-}
+          fun CreateProfessionalDTO.toEntity(): Professional= Professional(
+                  this.contactInfo.toEntity(),
+                  this.location,
+                  this.skills,
+                  this.dailyRate,
+                  this.employmentState,
+                  this.notes
+          )
--- CRM/src/main/kotlin/it/polito/wa2/g07/crm/dtos/lab03/ProfessionalDTO.kt
+++ CRM/src/main/kotlin/it/polito/wa2/g07/crm/dtos/lab03/ProfessionalDTO.kt
@@ -1,8 +1,10 @@
 package it.polito.wa2.g07.crm.dtos.lab03
 
 import it.polito.wa2.g07.crm.dtos.lab02.ContactDTO
+import it.polito.wa2.g07.crm.dtos.lab02.toContactDto
 
 import it.polito.wa2.g07.crm.entities.lab03.EmploymentState
+import it.polito.wa2.g07.crm.entities.lab03.Professional
 
 data class ProfessionalDTO(
     val id:Long,
@@ -13,6 +15,14 @@ data class ProfessionalDTO(
     var employmentState: EmploymentState,
     var notes: String?
 )
-{
 
-}
+fun Professional.toProfessionalDto(): ProfessionalDTO=
+    ProfessionalDTO(
+        this.professionalId,
+        this.contactInfo.toContactDto(),
+        this.location,
+        this.skills,
+        this.daily_rate,
+        this.employmentState,
+        this.notes
+    )
