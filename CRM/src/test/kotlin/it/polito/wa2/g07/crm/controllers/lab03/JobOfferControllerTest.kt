a42c7cb478f44c931a54ac8c1eb4686c3b6d0899
Alberto Verna <alberto.verna@hotmail.com>
2024-05-09 13:31:25 +0200

    Add unit tests for offer status update endpoint

--- CRM/src/test/kotlin/it/polito/wa2/g07/crm/controllers/lab03/JobOfferControllerTest.kt
+++ CRM/src/test/kotlin/it/polito/wa2/g07/crm/controllers/lab03/JobOfferControllerTest.kt
@@ -1,10 +1,177 @@
 package it.polito.wa2.g07.crm.controllers.lab03
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import it.polito.wa2.g07.crm.dtos.lab03.JobOfferUpdateDTO
+import it.polito.wa2.g07.crm.dtos.lab03.toJobOfferDTO
+import it.polito.wa2.g07.crm.entities.lab02.Contact
+import it.polito.wa2.g07.crm.entities.lab02.ContactCategory
+import it.polito.wa2.g07.crm.entities.lab03.Customer
+import it.polito.wa2.g07.crm.entities.lab03.JobOffer
+import it.polito.wa2.g07.crm.entities.lab03.OfferStatus
+import it.polito.wa2.g07.crm.entities.lab03.Professional
+import it.polito.wa2.g07.crm.exceptions.EntityNotFoundException
+import it.polito.wa2.g07.crm.exceptions.InvalidParamsException
+import it.polito.wa2.g07.crm.services.lab03.JobOfferService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
 
 @WebMvcTest(JobOfferController::class)
 class JobOfferControllerTest(@Autowired val mockMvc: MockMvc) {
+
+    @MockkBean
+    private lateinit var jobOfferService: JobOfferService
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Nested
+    inner class UpdateJobOfferTests {
+
+        private val mockJobOffer = JobOffer(
+            requiredSkills = mutableSetOf("skill1", "skill2"),
+            30,
+            "This is a description"
+        )
+
+        private val mockCustomer = Customer(
+            Contact(
+                "Mario",
+                "Rossi",
+                ContactCategory.CUSTOMER,
+                "RSSMRA70A01L219K"
+            ),
+            "mock Customer"
+        )
+
+        private val mockProfessional = Professional(
+            Contact(
+                "Mario",
+                "Rossi",
+                ContactCategory.CUSTOMER,
+                "RSSMRA70A01L219K"
+            ),
+            "Torino",
+            skills = mutableSetOf("skill1", "skill2"),
+            100.0
+        )
+
+        init {
+            mockCustomer.addPlacement(mockJobOffer)
+            mockJobOffer.offerId = 1L
+        }
+
+        @BeforeEach
+        fun initMocks() {
+            every { jobOfferService.updateJobOfferStatus(any(Long::class), any(JobOfferUpdateDTO::class)) } throws EntityNotFoundException("Job offer not found")
+            every { jobOfferService.updateJobOfferStatus(mockJobOffer.offerId, any(JobOfferUpdateDTO::class)) } answers {
+                val jobOfferUpdateDTO = secondArg<JobOfferUpdateDTO>()
+                mockJobOffer.status = jobOfferUpdateDTO.status
+                if(jobOfferUpdateDTO.status == OfferStatus.CANDIDATE_PROPOSAL) {
+                    mockJobOffer.professional = mockProfessional
+                }
+                mockJobOffer.toJobOfferDTO()
+            }
+            every { jobOfferService.updateJobOfferStatus(mockJobOffer.offerId, match { !mockJobOffer.status.canUpdateTo(it.status) }) } throws InvalidParamsException("Cannot update to status")
+            every { jobOfferService.updateJobOfferStatus(mockJobOffer.offerId, match { it.status == OfferStatus.CANDIDATE_PROPOSAL && it.professionalId != mockProfessional.professionalId }) } throws EntityNotFoundException("Professional not found")
+            every { jobOfferService.updateJobOfferStatus(mockJobOffer.offerId, match { it.status == OfferStatus.CANDIDATE_PROPOSAL && it.professionalId == null }) } throws InvalidParamsException("Professional not given")
+        }
+
+        @BeforeEach
+        fun resetJobOfferStatus() {
+            mockJobOffer.status = OfferStatus.CREATED
+            mockJobOffer.professional = null
+        }
+
+        @Test
+        fun updateJobOfferStatus_noProfessional() {
+            mockMvc
+                .post("/API/joboffers/${mockJobOffer.offerId}") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = objectMapper.writeValueAsString(JobOfferUpdateDTO(OfferStatus.SELECTION_PHASE))
+                }.andExpect {
+                    status { isCreated() }
+                    content {
+                        jsonPath("$.id") { value(mockJobOffer.offerId) }
+                        jsonPath("$.offerStatus") { value(OfferStatus.SELECTION_PHASE.toString()) }
+                        jsonPath("$.professional") { doesNotExist() }
+                    }
+                }
+        }
+
+        @Test
+        fun updateJobOfferStatus_withProfessional() {
+            mockJobOffer.status = OfferStatus.SELECTION_PHASE
+
+            mockMvc
+                .post("/API/joboffers/${mockJobOffer.offerId}") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = objectMapper.writeValueAsString(JobOfferUpdateDTO(OfferStatus.CANDIDATE_PROPOSAL, mockProfessional.professionalId))
+                }.andExpect {
+                    status { isCreated() }
+                    content {
+                        jsonPath("$.id") { value(mockJobOffer.offerId) }
+                        jsonPath("$.offerStatus") { value(OfferStatus.CANDIDATE_PROPOSAL.toString()) }
+                        jsonPath("$.professional.id") { value(mockProfessional.professionalId) }
+                    }
+                }
+        }
+
+        @Test
+        fun updateJobOfferStatus_invalidStatus() {
+            mockMvc
+                .post("/API/joboffers/${mockJobOffer.offerId}") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = objectMapper.writeValueAsString(JobOfferUpdateDTO(OfferStatus.DONE))
+                }.andExpect {
+                    status { isBadRequest() }
+                }
+        }
+
+        @Test
+        fun updateJobOfferStatus_offerNotFound() {
+            mockMvc
+                .post("/API/joboffers/${mockJobOffer.offerId + 1}") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = objectMapper.writeValueAsString(JobOfferUpdateDTO(OfferStatus.DONE))
+                }.andExpect {
+                    status { isNotFound() }
+                }
+        }
+
+        @Test
+        fun updateJobOfferStatus_professionalNotGiven() {
+            mockJobOffer.status = OfferStatus.SELECTION_PHASE
+
+            mockMvc
+                .post("/API/joboffers/${mockJobOffer.offerId}") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = objectMapper.writeValueAsString(JobOfferUpdateDTO(OfferStatus.CANDIDATE_PROPOSAL))
+                }.andExpect {
+                    status { isBadRequest() }
+                }
+        }
+
+        @Test
+        fun updateJobOfferStatus_professionalNotFound() {
+            mockJobOffer.status = OfferStatus.SELECTION_PHASE
+
+            mockMvc
+                .post("/API/joboffers/${mockJobOffer.offerId}") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = objectMapper.writeValueAsString(JobOfferUpdateDTO(OfferStatus.CANDIDATE_PROPOSAL, mockProfessional.professionalId + 1))
+                }.andExpect {
+                    status { isNotFound() }
+                }
+        }
+
+    }
 
 }
--- CRM/src/test/kotlin/it/polito/wa2/g07/crm/services/lab03/JobOfferServiceTest.kt
+++ CRM/src/test/kotlin/it/polito/wa2/g07/crm/services/lab03/JobOfferServiceTest.kt
@@ -3,28 +3,32 @@ package it.polito.wa2.g07.crm.services.lab03
 import io.mockk.*
 
 import it.polito.wa2.g07.crm.dtos.lab02.ReducedContactDTO
+import it.polito.wa2.g07.crm.dtos.lab03.*
 
-import it.polito.wa2.g07.crm.dtos.lab03.JobOfferCreateDTO
-import it.polito.wa2.g07.crm.dtos.lab03.JobOfferDTO
-import it.polito.wa2.g07.crm.dtos.lab03.ReducedCustomerDTO
 import it.polito.wa2.g07.crm.entities.lab02.Contact
 import it.polito.wa2.g07.crm.entities.lab02.ContactCategory
 import it.polito.wa2.g07.crm.entities.lab03.Customer
 import it.polito.wa2.g07.crm.entities.lab03.JobOffer
 import it.polito.wa2.g07.crm.entities.lab03.OfferStatus
+import it.polito.wa2.g07.crm.entities.lab03.Professional
 import it.polito.wa2.g07.crm.exceptions.EntityNotFoundException
+import it.polito.wa2.g07.crm.exceptions.InvalidParamsException
 
 import it.polito.wa2.g07.crm.repositories.lab03.CustomerRepository
 import it.polito.wa2.g07.crm.repositories.lab03.JobOfferRepository
+import it.polito.wa2.g07.crm.repositories.lab03.ProfessionalRepository
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import java.util.*
 
 class JobOfferServiceTest {
 
     private val customerRepository= mockk<CustomerRepository>()
     private val jobOfferRepository= mockk<JobOfferRepository>()
-    private val service = JobOfferServiceImpl(jobOfferRepository,customerRepository)
+    private val professionalRepository = mockk<ProfessionalRepository>()
+
+    private val service = JobOfferServiceImpl(jobOfferRepository,customerRepository,professionalRepository)
     @Nested
     inner class CreateJobOffer(){
         private val jobOfferSlot = slot<JobOffer>()
@@ -84,6 +88,128 @@ class JobOfferServiceTest {
             verify{ jobOfferRepository.save(any(JobOffer::class)) wasNot called}
             verify (exactly = 1) { customerRepository.findById(any(Long::class))  }
 
+        }
+
+    }
+
+    @Nested
+    inner class UpdateJobOfferTests {
+
+        private val mockJobOffer = JobOffer(
+            requiredSkills = mutableSetOf("skill1", "skill2"),
+            30,
+            "This is a description"
+        )
+
+        private val mockCustomer = Customer(
+            Contact(
+                "Mario",
+                "Rossi",
+                ContactCategory.CUSTOMER,
+                "RSSMRA70A01L219K"
+            ),
+            "mock Customer"
+        )
+
+        private val mockProfessional = Professional(
+            Contact(
+                "Mario",
+                "Rossi",
+                ContactCategory.CUSTOMER,
+                "RSSMRA70A01L219K"
+            ),
+            "Torino",
+            skills = mutableSetOf("skill1", "skill2"),
+            100.0
+        )
+
+        init {
+            mockCustomer.addPlacement(mockJobOffer)
+            mockJobOffer.offerId = 1L
+        }
+
+        @BeforeEach
+        fun initMocks() {
+            every { jobOfferRepository.save(any(JobOffer::class)) } answers { firstArg<JobOffer>() }
+            every { jobOfferRepository.findById(any(Long::class)) } returns Optional.empty()
+            every { jobOfferRepository.findById(mockJobOffer.offerId) } returns Optional.of(mockJobOffer)
+            every { professionalRepository.findById(any(Long::class)) } returns Optional.empty()
+            every { professionalRepository.findById(mockProfessional.professionalId) } returns Optional.of(mockProfessional)
+        }
+
+        @BeforeEach
+        fun resetJobOfferStatus() {
+            mockJobOffer.status = OfferStatus.CREATED
+            mockJobOffer.professional = null
+        }
+
+        @Test
+        fun updateJobOfferStatus_validStatus_noProfessional() {
+            val result = service.updateJobOfferStatus(mockJobOffer.offerId, JobOfferUpdateDTO(OfferStatus.SELECTION_PHASE))
+
+            assertEquals(result, mockJobOffer.toJobOfferDTO())
+            verify { jobOfferRepository.save(any(JobOffer::class)) }
+        }
+
+        @Test
+        fun updateJobOfferStatus_validStatus_withProfessional() {
+            mockJobOffer.status = OfferStatus.SELECTION_PHASE
+            val result = service.updateJobOfferStatus(mockJobOffer.offerId, JobOfferUpdateDTO(OfferStatus.CANDIDATE_PROPOSAL, mockProfessional.professionalId))
+
+            assertEquals(result, mockJobOffer.toJobOfferDTO())
+            assertEquals(result.professional, mockProfessional.toProfessionalReducedDTO_Basic())
+            verify { jobOfferRepository.save(any(JobOffer::class)) }
+        }
+
+        @Test
+        fun updateJobOfferStatus_removeProfessional() {
+            mockJobOffer.status = OfferStatus.CONSOLIDATED
+            mockJobOffer.professional = mockProfessional
+
+            val result = service.updateJobOfferStatus(mockJobOffer.offerId, JobOfferUpdateDTO(OfferStatus.SELECTION_PHASE))
+            assertNull(result.professional)
+            verify { jobOfferRepository.save(any(JobOffer::class)) }
+        }
+
+        @Test
+        fun updateJobOfferStatus_invalidStatus() {
+            assertThrows<InvalidParamsException> {
+                service.updateJobOfferStatus(mockJobOffer.offerId, JobOfferUpdateDTO(OfferStatus.DONE))
+            }
+
+            verify(exactly = 0) { jobOfferRepository.save(any(JobOffer::class)) }
+        }
+
+        @Test
+        fun updateJobOfferStatus_offerNotFound() {
+            assertThrows<EntityNotFoundException> {
+                service.updateJobOfferStatus(mockJobOffer.offerId + 1, JobOfferUpdateDTO(OfferStatus.SELECTION_PHASE))
+            }
+
+            verify(exactly = 0) { jobOfferRepository.save(any(JobOffer::class)) }
+        }
+
+        @Test
+        fun updateJobOfferStatus_professionalNotGiven() {
+            mockJobOffer.status = OfferStatus.SELECTION_PHASE
+
+            assertThrows<InvalidParamsException> {
+                service.updateJobOfferStatus(mockJobOffer.offerId, JobOfferUpdateDTO(OfferStatus.CANDIDATE_PROPOSAL))
+            }
+
+            verify(exactly = 0) { jobOfferRepository.save(any(JobOffer::class)) }
+        }
+
+
+        @Test
+        fun updateJobOfferStatus_professionalNotFound() {
+            mockJobOffer.status = OfferStatus.SELECTION_PHASE
+
+            assertThrows<EntityNotFoundException> {
+                service.updateJobOfferStatus(mockJobOffer.offerId, JobOfferUpdateDTO(OfferStatus.CANDIDATE_PROPOSAL, mockProfessional.professionalId + 1))
+            }
+
+            verify(exactly = 0) { jobOfferRepository.save(any(JobOffer::class)) }
         }
 
     }
