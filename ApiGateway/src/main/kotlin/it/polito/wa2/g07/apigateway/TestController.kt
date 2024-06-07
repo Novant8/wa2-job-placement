ad0274edebc6f532567990eb380928e207a4b521
vitto-sanf <vittorio.sanfilippo00@gmail.com>
2024-06-07 15:12:54 +0200

    connect frontend to API Gateway

--- ApiGateway/src/main/kotlin/it/polito/wa2/g07/apigateway/TestController.kt
+++ ApiGateway/src/main/kotlin/it/polito/wa2/g07/apigateway/TestController.kt
@@ -1,6 +1,8 @@
 package it.polito.wa2.g07.apigateway
 
 import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
+import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -19,6 +21,27 @@ class TestController {
         return mapOf(
             "result" to "OK",
             "authentication" to authentication
+        )
+    }
+
+    @GetMapping("/me")
+    fun me (
+        @CookieValue(name="XSRF-TOKEN", required = false)
+        xsrf: String?,
+        authentication: Authentication?
+    ):Map<String,Any?>{
+        val principal :OidcUser? = authentication?.principal as? OidcUser
+        val name = principal?.givenName ?:""
+        val surname = principal?.familyName ?: ""
+        val role = principal?.authorities ?: ""
+        return mapOf(
+            "name" to name,
+            "surname" to surname,
+            "loginUrl" to "/oauth2/authorization/crmclient",
+            "logoutUrl" to "/logout",
+            "principal" to principal,
+            "xsrfToken" to xsrf,
+            "role" to role
         )
     }
 
