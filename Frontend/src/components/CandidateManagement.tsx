5d145999a13c2abebce9a8268ba76a4dfa126280
SalvatoreCavallaro1 <135614562+SalvatoreCavallaro1@users.noreply.github.com>
2024-08-07 13:19:25 +0200

    using correcr professional type

--- Frontend/src/components/CandidateManagement.tsx
+++ Frontend/src/components/CandidateManagement.tsx
@@ -3,19 +3,21 @@ import API from "../../API.tsx";
 import {useAuth} from "../contexts/auth.tsx";
 import {Accordion, Container,InputGroup,Form} from "react-bootstrap";
 import * as Icon from 'react-bootstrap-icons';
-
+import {Professional} from "../types/professional.ts";
 
 //TODO: modify the filter system in order to use the API and not the filtering on the frontend
 
 export type ProfessionalAccordionProps = {
-    prof: {
+    prof: Professional/*{
 
         "id":number,
         "contactInfo":
             {"id":number,"name":string,"surname":string,"category":string},
         "location":string,
         "skills": string[],
-        "employmentState":string}
+        "employmentState":string,
+        notes? :string
+    }*/
 
 };
 
@@ -30,6 +32,12 @@ function ProfessionalAccordion(props: ProfessionalAccordionProps) {
                     <div>
                         Category: {props.prof.contactInfo.category}
                     </div>
+                    {props.prof.contactInfo.ssn?
+                        <div>
+                            SSN: {props.prof.contactInfo.ssn}
+                        </div>
+                    :""}
+                    {/*TODO: mostrare gli addresses della contact info*/}
                     <div>
                         Location: {props.prof.location}
                     </div>
@@ -46,8 +54,12 @@ function ProfessionalAccordion(props: ProfessionalAccordionProps) {
                     }
                     </div>
                     <div>
-                        Employment State:{props.prof.employmentState}
+                        Employment State:{props.prof.employedState}
                     </div>
+                    {props.prof.notes?
+                    <div>Notes: {props.prof.notes}</div>
+                    :""
+                    }
                 </Accordion.Body>
 
             </Accordion.Item>
@@ -59,28 +71,37 @@ export  default  function CandidateManagement(){
 
 
 
-    const[candidates,setCandidates] = useState( {"content":
+    /*const[candidates,setCandidates] = useState( {"content":
             [
                 {
                     "id":1,
-                    "contactInfo":{"id":1,"name":"Luigi","surname":"Verdi","category":"PROFESSIONAL"},
+                    "contactInfo":{"id":1,"name":"Luigi","surname":"Verdi","category":"PROFESSIONAL",
+                    "addresses":[]
+                    },
                     "location":"Torino",
                     "skills": ["Proficient in Kotlin","Can work well in a team"],
-                    "employmentState":"UNEMPLOYED"
+                    "dailyRate" : 20,
+                    "employedState":"UNEMPLOYED"
                 },
                 {
                     "id":2,
-                    "contactInfo":{"id":2,"name":"Mario","surname":"Rossi","category":"PROFESSIONAL"},
+                    "contactInfo":{"id":2,"name":"Mario","surname":"Rossi","category":"PROFESSIONAL",
+                    "addresses":[]
+                    },
                     "location":"Milano",
                     "skills": ["Proficient in Java","Can work well in a team","Agile"],
-                    "employmentState":"EMPLOYED"
+                    "dailyRate" : 20,
+                    "employedState":"EMPLOYED"
                 },
                 {
                     "id":3,
-                    "contactInfo":{"id":3,"name":"Giovanni","surname":"Mariani","category":"PROFESSIONAL"},
+                    "contactInfo":{"id":3,"name":"Giovanni","surname":"Mariani","category":"PROFESSIONAL",
+                    "addresses":[]
+                    },
                     "location":"Bologna",
                     "skills": ["Proficient in Python","Can work well in a team","Agile","Mobile Application"],
-                    "employmentState":"UNEMPLOYED"
+                    "dailyRate" : 20,
+                    "employedState":"UNEMPLOYED"
                 }
 
             ],
@@ -92,25 +113,25 @@ export  default  function CandidateManagement(){
         "sort":{"empty":true,"sorted":false,"unsorted":true},
         "numberOfElements":3,
         "first":true,
-        "empty":false})
+        "empty":false})*/
 
     const {me} = useAuth()
     //console.log(me);
     const [professional, setProfessional]=useState({});
     const [searchTerm, setSearchTerm] = useState("");
 
-    const handleSearchChange = (event) => {
+    const handleSearchChange = (event:any) => {
         setSearchTerm(event.target.value);
     };
 
 
-    const filteredCandidates = candidates.content.filter(candidate => {
+    /*const filteredCandidates = candidates.content.filter(candidate => {
         const nameMatch = candidate.contactInfo.name.toLowerCase().includes(searchTerm.toLowerCase());
         const surnameMatch = candidate.contactInfo.surname.toLowerCase().includes(searchTerm.toLowerCase());
         const locationMatch = candidate.location.toLowerCase().includes(searchTerm.toLowerCase());
         const skillsMatch = candidate.skills.some(skill => skill.toLowerCase().includes(searchTerm.toLowerCase()));
         return nameMatch || surnameMatch || locationMatch || skillsMatch;
-    });
+    });*/
 
     useEffect(() => {
         const token = me?.xsrfToken
@@ -144,11 +165,11 @@ export  default  function CandidateManagement(){
                 <Accordion>
                     {/*candidates && candidates.content.map((e) =>
                             <ProfessionalAccordion key={e.id} prof={e}/>*/
-                        /*professional && professional.content?.map((e)=>
-                            <ProfessionalAccordion key={e.id} prof={e}/>*/
+                        professional && professional.content?.map((e)=>
+                            <ProfessionalAccordion key={e.id} prof={e}/>
 
-                        candidates && filteredCandidates.map((e) =>
-                        <ProfessionalAccordion key={e.id} prof={e}/>
+                       /* candidates && filteredCandidates.map((e) =>
+                        <ProfessionalAccordion key={e.id} prof={e}/>*/
                     )}
 
                 </Accordion>
