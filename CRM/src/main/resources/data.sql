91d590967ddd30bf10f0209b0eab2cb94e2cc627
SalvatoreCavallaro1 <135614562+SalvatoreCavallaro1@users.noreply.github.com>
2024-08-26 13:06:37 +0200

    Update data.sql

--- CRM/src/main/resources/data.sql
+++ CRM/src/main/resources/data.sql
@@ -54,3 +54,105 @@ BEGIN
     END IF;
 END;
 ';
+-- Luca Rossi
+DO '
+BEGIN
+    IF (SELECT count(*) FROM contact WHERE user_id = ''a8e0a84c-a3b5-4965-9cac-cdb0cdfacd9b'') = 0 THEN
+        INSERT INTO contact(contact_id, name, surname, user_id, category)
+        VALUES (nextval(''contact_seq''), ''Luca'', ''Rossi'', ''a8e0a84c-a3b5-4965-9cac-cdb0cdfacd9b'', 1);
+
+        INSERT INTO address(id, channel)
+        VALUES (nextval(''address_seq''), ''email'');
+
+        INSERT INTO email(id, email)
+        VALUES (currval(''address_seq''), ''luca.rossi@example.org'');
+
+        INSERT INTO contact_addresses(contacts_contact_id, addresses_id)
+        VALUES (currval(''contact_seq''), currval(''address_seq''));
+
+        INSERT INTO professional(professional_id, contact_info_contact_id, daily_rate, employment_state, location)
+        VALUES (nextval(''professional_seq''), currval(''contact_seq''), 0, 0, ''Torino'');
+
+        INSERT INTO professional_skills(professional_professional_id, skills)
+        VALUES (currval(''professional_seq''), ''Proficient in Java'');
+
+        INSERT INTO professional_skills(professional_professional_id, skills)
+        VALUES (currval(''professional_seq''), ''Work in Team'');
+
+
+
+
+
+
+    END IF;
+END;
+';
+-- Giovanni Mariani
+DO '
+BEGIN
+    IF (SELECT count(*) FROM contact WHERE user_id = ''64ee56f9-d14b-4b94-9f6a-9d2a20cb5d18'') = 0 THEN
+        INSERT INTO contact(contact_id, name, surname, user_id, category)
+        VALUES (nextval(''contact_seq''), ''Giovanni'', ''Mariani'', ''64ee56f9-d14b-4b94-9f6a-9d2a20cb5d18'', 1);
+
+        INSERT INTO address(id, channel)
+        VALUES (nextval(''address_seq''), ''email'');
+
+        INSERT INTO email(id, email)
+        VALUES (currval(''address_seq''), ''giovanni.mariani@example.org'');
+
+        INSERT INTO contact_addresses(contacts_contact_id, addresses_id)
+        VALUES (currval(''contact_seq''), currval(''address_seq''));
+
+        INSERT INTO professional(professional_id, contact_info_contact_id, daily_rate, employment_state, location)
+        VALUES (nextval(''professional_seq''), currval(''contact_seq''), 0, 0, ''Milano'');
+
+        INSERT INTO professional_skills(professional_professional_id, skills)
+        VALUES (currval(''professional_seq''), ''Proficient in Kotlin'');
+
+        INSERT INTO professional_skills(professional_professional_id, skills)
+        VALUES (currval(''professional_seq''), ''Work in Team'');
+
+        INSERT INTO professional_skills(professional_professional_id, skills)
+        VALUES (currval(''professional_seq''), ''Mobile Application'');
+    END IF;
+END;
+';
+-- Silvio Pellico
+DO '
+BEGIN
+    IF (SELECT count(*) FROM contact WHERE user_id = ''631e0f6f-8f06-4007-af3c-5a823c3aae52'') = 0 THEN
+        INSERT INTO contact(contact_id, name, surname, user_id, category)
+        VALUES (nextval(''contact_seq''), ''Silvio'', ''Pellico'', ''631e0f6f-8f06-4007-af3c-5a823c3aae52'', 2);
+
+        INSERT INTO address(id, channel)
+        VALUES (nextval(''address_seq''), ''email'');
+
+        INSERT INTO email(id, email)
+        VALUES (currval(''address_seq''), ''silvio.pellico@example.org'');
+
+        INSERT INTO contact_addresses(contacts_contact_id, addresses_id)
+        VALUES (currval(''contact_seq''), currval(''address_seq''));
+    END IF;
+END;
+';
+-- Company A
+DO '
+BEGIN
+    IF (SELECT count(*) FROM contact WHERE user_id = ''1f1c5579-1d62-47fd-9676-c4f8cf9d5593'') = 0 THEN
+        INSERT INTO contact(contact_id, name, surname, user_id, category)
+        VALUES (nextval(''contact_seq''), ''Company'', ''A'', ''1f1c5579-1d62-47fd-9676-c4f8cf9d5593'', 0);
+
+        INSERT INTO address(id, channel)
+        VALUES (nextval(''address_seq''), ''email'');
+
+        INSERT INTO email(id, email)
+        VALUES (currval(''address_seq''), ''company.a@example.org'');
+
+        INSERT INTO contact_addresses(contacts_contact_id, addresses_id)
+        VALUES (currval(''contact_seq''), currval(''address_seq''));
+
+        INSERT INTO customer(customer_id, contact_info_contact_id)
+        VALUES (nextval(''customer_seq''), currval(''contact_seq''));
+    END IF;
+END;
+';
